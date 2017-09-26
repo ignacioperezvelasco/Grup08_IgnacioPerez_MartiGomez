6a4acba718c8992b017beb42bb7721415c38dcfc
Martí Gómez <magopru@hotmail.es>
2017-09-26 12:38:54 +0200

    Iteradores hacia adelante

--- A01_IgnacioPerez_MartiGomez/A01IgnacioPerezMartiGomez.cpp
+++ A01_IgnacioPerez_MartiGomez/A01IgnacioPerezMartiGomez.cpp
@@ -9,6 +9,7 @@
 #include <forward_list>
 #include <map>
 #include <set>
+#include <iterator>
 
 
 void main()
@@ -18,24 +19,72 @@ void main()
 	std::deque<int> deque({ 1,2,3,4,5 });
 	std::list<int> list({ 1,2,3,4,5 });
 	std::forward_list<int> forward_list({ 1,2,3,4,5 });
-	std::map<std::string,int> map;
-	map["uno"] = 1;
-	map["dos"] = 2;
-	map["tres"] = 3;
-	map["cuatro"] =4 ;
-	map["cinco"] = 5;
+	std::map<char,int> map;
+	map['a'] = 1;
+	map['b'] = 2;
+	map['c'] = 3;
+	map['d'] = 4;
+	map['e'] = 5;
 	std::set<int> set({ 1,2,3,4,5 });
 
     ////////////////////////////////////////
 
-	using  myVectorIterator = std::vector<int>::iterator;
-	using  myDequeIterator = std::deque<int>::iterator;
-	using  myListIterator = std::list<int>::iterator;
-	using  myForward_listIterator = std::forward_list<int>::iterator;
-	using  myMapIterator = std::map<std::string,int>::iterator;
-	using  mySetIterator = std::set<int>::iterator;
+	using myVectorIterator = std::vector<int>::iterator;
+	using myDequeIterator = std::deque<int>::iterator;
+	using myListIterator = std::list<int>::iterator;
+	using myForward_listIterator = std::forward_list<int>::iterator;
+	using myMapIterator = std::map<char,int>::iterator;
+	using mySetIterator = std::set<int>::iterator;
 
 	/////////////////////////////////////////
+	//RECORRIDO DE BUCLES
+	//VECTOR
+	std::cout << "VECTOR: ";
+	for (myVectorIterator it = vector.begin(); it != vector.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	//DEQUE
+	std::cout << "DEQUE: ";
+	for (myDequeIterator it = deque.begin(); it != deque.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	//LIST
+	std::cout << "LIST: ";
+	for (myListIterator it = list.begin(); it != list.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	//FORWARD_LIST
+	std::cout << "FORWARD_LIST: ";
+	for (myForward_listIterator it = forward_list.begin(); it != forward_list.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	//MAP
+	std::cout << "MAP: ";
+	for (myMapIterator it = map.begin(); it != map.end(); it++)
+	{
+		std::cout << it->first << "=" << it->second << " ";
+	}
+	std::cout << std::endl;
+
+	//SET
+	std::cout << "SET: ";
+	for (mySetIterator it = set.begin(); it != set.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 
 }
 
