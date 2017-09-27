dfa3016c18dde9c4e1e084eef34a585b0a49f3e6
Martí Gómez <magopru1995@gmail.com>
2017-09-27 13:18:31 +0200

    Prueba a ver

--- A01_IgnacioPerez_MartiGomez/A01IgnacioPerezMartiGomez.cpp
+++ A01_IgnacioPerez_MartiGomez/A01IgnacioPerezMartiGomez.cpp
@@ -15,6 +15,7 @@
 void main()
 {
 	///////////////////////////////////////////
+	//STDs
 	std::vector<int> vector({ 1,2,3,4,5});
 	std::deque<int> deque({ 1,2,3,4,5 });
 	std::list<int> list({ 1,2,3,4,5 });
@@ -28,7 +29,7 @@ void main()
 	std::set<int> set({ 1,2,3,4,5 });
 
     ////////////////////////////////////////
-
+	//ITERATORS
 	using myVectorIterator = std::vector<int>::iterator;
 	using myDequeIterator = std::deque<int>::iterator;
 	using myListIterator = std::list<int>::iterator;
@@ -36,27 +37,55 @@ void main()
 	using myMapIterator = std::map<char,int>::iterator;
 	using mySetIterator = std::set<int>::iterator;
 
+	//REVERSE ITERATORS
+	using myReverseVectorIterator = std::vector<int>::reverse_iterator;
+	using myReverseDequeIterator = std::deque<int>::reverse_iterator;
+	using myReverseListIterator = std::list<int>::reverse_iterator;
+	using myReverseMapIterator = std::map<int>::reverse_iterator;
+	using myReverseSetIterator = std::set<int>::reverse_iterator;
+
 	/////////////////////////////////////////
 	//RECORRIDO DE BUCLES
 	//VECTOR
-	std::cout << "VECTOR: ";
+	std::cout << "VECTOR ITERATOR: ";
 	for (myVectorIterator it = vector.begin(); it != vector.end(); it++)
 	{
 		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
 
+	std::cout << "VECTOR REVERSE_ITERATOR: ";
+	for (myReverseVectorIterator it = vector.rbegin(); it != vector.rend(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
 	//DEQUE
-	std::cout << "DEQUE: ";
+	std::cout << "DEQUE ITERATOR: ";
 	for (myDequeIterator it = deque.begin(); it != deque.end(); it++)
 	{
 		std::cout << *it << " ";
 	}
 	std::cout << std::endl;
 
+	std::cout << "DEQUE REVERSE_ITERATOR: ";
+	for (myReverseDequeIterator it = deque.rbegin(); it != deque.rend(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
 	//LIST
-	std::cout << "LIST: ";
+	std::cout << "LIST ITERATOR: ";
 	for (myListIterator it = list.begin(); it != list.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "LIST REVERSE_ITERATOR: ";
+	for (myReverseListIterator it = list.rbegin(); it != list.rend(); it++)
 	{
 		std::cout << *it << " ";
 	}
@@ -78,9 +107,23 @@ void main()
 	}
 	std::cout << std::endl;
 
+	std::cout << "MAP REVERSE_ITERATOR: ";
+	for (myReverseMapIterator it = map.rbegin(); it != map.rend(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
 	//SET
-	std::cout << "SET: ";
+	std::cout << "SET ITERATOR: ";
 	for (mySetIterator it = set.begin(); it != set.end(); it++)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
+	std::cout << "SET REVERSE_ITERATOR: ";
+	for (myReverseSetIterator it = set.rbegin(); it != set.rend(); it++)
 	{
 		std::cout << *it << " ";
 	}
